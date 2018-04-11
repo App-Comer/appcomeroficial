1c3780afa56e6242589ddd494ba6dbfa6912245f
armandocano91 <armandocano.ac@gmail.com>
2018-04-11 09:43:43 -0500

    comented out search Map, Yelp and table list. Console is clear

--- src/lib/searchYelp.js
+++ src/lib/searchYelp.js
@@ -1,9 +1,9 @@
 // //setting up var as URL
 // var yelpURL = 'https://api.yelp.com/v3/businesses/search';
 // //making a GET request to bring data from Yelp's API
-var searchYelp = ({key, query, max =5}, callback) =>{
+// var searchYelp = ({key, query, max =5}, callback) =>{
 //   //options object with properties required
 // };
 
 //this is how you can access to this library from other components
-window.searchYelp = searchYelp;
+// window.searchYelp = searchYelp;
